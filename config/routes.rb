7428e7d88ce76b9d327db1b1ee2b15c9c5f31abd
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-10 17:03:12 +0000

    Show, create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # create
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+
   get "articles", to: "articles#index"
+  get "articles/:id", to: "articles#show", as: "article"
 end
