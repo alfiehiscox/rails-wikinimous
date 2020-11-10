99e95ca59139a9cf561d8b150de843ff8295241f
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-10 19:01:11 +0000

    update and delete methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # create
   get "articles/new", to: "articles#new"
-  post "articles", to: "articles#create"
 
   get "articles", to: "articles#index"
+  post "articles", to: "articles#create"
+
   get "articles/:id", to: "articles#show", as: "article"
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy"
+
+  get "article/:id/edit", to: "articles#edit" , as: "article-edit"
 end
